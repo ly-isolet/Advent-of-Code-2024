33ab35a3682832c0a657f09c42a9a959b011765e
Cameron Kimmel <cameron@thekimmels.com>
2024-12-15 13:28:51 -0600

    more work on p7

--- Day 7/q7.py
+++ Day 7/q7.py
@@ -13,28 +13,32 @@ def parseInput(name):
     numbers = []
     # split the file input into two arrays to hold each side of the 'equation'
     for line in file:
-        splitLine = line.split(':')
+        splitLine = line.split(': ')
         answers.append(splitLine[0])
         numbers.append(splitLine[1])
     
     return answers, numbers
 
-# Takes two list[int] and calculates if the right side numbers can combine (add / mult) to equal the left number 
-def mathMoment(leftside, rightside):
-    # valids = []
-    totalValid = 0
-
-    for num in leftside:
-        isValid = False
-        ## CONTINUE HERE ##
-        
-    return totalValid
+# Recursion
+def search(cur, rightside, leftside):
+    if len(rightside) == 0:
+        return cur == leftside
+    if cur > leftside:
+        return False
+    temp = rightside[1:]
+    ## p1
+    return search(cur + rightside[0], temp, leftside) or search(cur * rightside[0], temp, leftside)
+    ## p2
+    # return search(cur + l[0], temp, target) or search(cur * l[0], temp, target) or search(int(str(cur) + str(l[0])), temp, target)
 
 
 
 # Driver section!
-parsedInput = parseInput('Day 7/d7.txt')
-validEquations = mathMoment(parsedInput[0], parsedInput[1])
-total = len(validEquations)
-
-# print(total)
+total = 0
+answers, numbers = parseInput('Day 7/q7.txt')
+for i in range(len(answers)):
+    answer = int(answers[i])
+    number = numbers[i]
+    if search(0, list(map(int, number.split(' '))), answer):
+        total += answer
+print(total)
